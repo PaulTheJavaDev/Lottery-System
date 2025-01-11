429a235578bdac28b68c4d9e99bfb3b929823696
Paul <Paul-Ludwig.Simon@t-online.de>
2025-01-11 14:30:56 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,67 +2,28 @@ import java.util.*;
 
 public class Main {
 
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        // Generate winning numbers
-        int[] winningNumbers = generateWinningNumbers();
-
-        checkUserNumbers(winningNumbers);
-    }
-
-    public static int[] generateWinningNumbers() {
-        Random random = new Random();
 
         while (true) {
-            int[] numbers = random.ints(6, 1, 50).toArray();
+            System.out.println("+-----------------------------------------------+");
+            System.out.println("|                 Lottery Menu                  |");
+            System.out.println("|               Select an Option                |");
+            System.out.println("+-----------------------------------------------+");
+            System.out.println("|  1. Purchase a ticket                         |");
+            System.out.println("|  2. Exit the Program                          |");
+            System.out.println("+-----------------------------------------------+");
 
-            // Check for duplicates
-            Set<Integer> uniqueNumbers = new HashSet<>(); //first time using HashSet <3
-            boolean hasDuplicates = Arrays.stream(numbers)
-                    .anyMatch(n -> !uniqueNumbers.add(n));
+            int userInput = scanner.nextInt();
+            scanner.nextLine();
 
-            //continues with the code (main method) if the user doesn't have any duplicate numbers
-            if (!hasDuplicates) {
-                System.out.println("Winning Numbers: " + Arrays.toString(numbers));
-                return numbers;
+            switch (userInput) {
+                case 1 -> Lottery.purchaseTicket();
+                case 2 -> System.exit(0);
+                default -> System.out.println("Please enter a valid number");
             }
         }
-    }
-
-    public static void checkUserNumbers(int[] winningNumbers) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter 6 unique numbers between 1 and 50:");
-
-        //HasSet creation:
-        //A HashSet is a collection of items where every item is unique, and it is found in the java.util package
-        Set<Integer> userNumbers = new HashSet<>();
-
-        while (userNumbers.size() < 6) {
-            try {
-                System.out.print("Enter number " + (userNumbers.size() + 1) + ": ");
-                int number = Integer.parseInt(scanner.nextLine());
-
-                //checks if the number from the user is a valid number (1-50)
-                if (number < 1 || number > 50) {
-                    System.out.println("Please enter a number between 1 and 50.");
-                } else if (!userNumbers.add(number)) {
-                    System.out.println("You already entered that number. Try again.");
-                }
-            } catch (Exception e) {
-                System.out.println("Invalid input. Please enter a number.");
-            }
-        }
-
-        System.out.println("Your Numbers: " + userNumbers);
-
-        // Check how many numbers match
-        long matchingCount = userNumbers.stream()
-                .filter(num -> Arrays.stream(winningNumbers).anyMatch(w -> w == num))
-                .count();
         
-        if (matchingCount == 6) {
-            System.out.println("Congratulations you won the Jackpot!");
-        } else {
-            System.out.println("You matched " + matchingCount + " numbers!");
-        }
     }
 }
